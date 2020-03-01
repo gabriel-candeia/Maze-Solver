b3e9015e5537fbe42e3c7daf944224d5ddb5be3f
Gabriel Candeia <gabrielcandeia.a@gmail.com>
2020-03-01 12:16:39 -0300

    Added plot function

--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,30 @@
-function draw_maze(maze){
-    //1 for walls and 0 for non-walls
-
+function draw_maze(maze, n, m){
     var canvas = document.getElementById("canvas");
     if(canvas.getContext){
         var ctx = canvas.getContext("2d");
-        var h=20, w=20;
+        var w=canvas.height/m, h=canvas.width/n;
+        ctx.clearRect(0,0,canvas.width,canvas.height);
 
-        for(let i=0;i<maze.length;i++){
-            for(let j=0;j<maze[i].length;j++){
-                if(maze[i][j]==1){
-                    ctx.fillRect(j*w,i*h,w,h);
+        
+        /*ctx.moveTo(0,0);
+        ctx.lineTo((m+1)*w,0);*/
+        for(let i=0;i<n;i++){
+            for(let j=0;j<m;j++){
+                if(maze[i*m+j].includes(i*m+j+1)==false && j!=m-1){
+                    ctx.beginPath();
+                    ctx.moveTo((j+1)*w,i*h);
+                    ctx.lineTo((j+1)*w,(i+1)*h);
+                    ctx.stroke();
+                }
+                if(maze[i*m+j].includes(i*m+j+m)==false && i!=n-1){
+                    ctx.beginPath();
+                    ctx.moveTo(j*w,(i+1)*h);
+                    ctx.lineTo((j+1)*w,(i+1)*h);
+                    ctx.stroke();
                 }
             }
         }
+        
     }
 }
 
@@ -22,20 +34,17 @@ function comparator(a,b){
 
 function generate_maze(n,m){
     //create edges and assign random weights to them
-    var edges = [];
+    let edges = [];
     for(let i=0;i<n-1;i++){
         for(let j=0;j<m-1;j++){
             edges.push({weight: Math.ceil(Math.random()*100), v: j+i*m,w: j+i*m+1});
             edges.push({weight: Math.ceil(Math.random()*100), v: j+i*m,w: j+i*m+m});
         }
+        edges.push({weight: Math.ceil(Math.random()*100), v: m-1+i*m,w: m-1+i*m+m});
     }
 
     for(let j=0;j<m-1;j++){
         edges.push({weight: Math.ceil(Math.random()*100), v: j+(n-1)*m,w: j+(n-1)*m+1});
-    }
-
-    for(let i=0;i<n-1;i++){
-        edges.push({weight: Math.ceil(Math.random()*100), v: m-1+i*m,w: m-1+i*m+m});
     }
 
     //create adj list
@@ -45,11 +54,9 @@ function generate_maze(n,m){
     }
 
     //find-minimum-spanning-tree and store it in a adj list
-    var aux = new DSU(n*m);
+    let aux = new DSU(n*m);
     edges.sort(comparator);
     for(i of edges){
-        console.log(i);
-        console.log(aux.isSameSet(i.v,i.w));
         if(!aux.isSameSet(i.v,i.w)){
             aux.union(i.v,i.w);
             adj[i.w].push(i.v);
@@ -58,10 +65,13 @@ function generate_maze(n,m){
     }
 
     return adj;
+}   
+
+function randomize_button(){
+    var n = Math.ceil(Math.random()*(50-15))+15;
+    draw_maze(generate_maze(n,n),n,n);
 }
 
-var maze = [ 
-            [1,1,1],
-            [1,0,0,1],
-            [1,1,1],
-];
+(function(){
+    document.getElementById("randomize-button").addEventListener("click",randomize_button,false);
+})()
